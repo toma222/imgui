e3c884f362319d25d4aceee048d685248956ebbd
basilly <aidannwilhite@gmail.com>
2024-09-08 11:22:14 -0500

    added the new and improved vulkan premake config

---
+++ premake5.lua
@@ -0,0 +1,60 @@
+
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"*.cpp",
+		"*.h",
+		
+		"backends/imgui_impl_vulkan.cpp",
+		"backends/imgui_impl_glfw.cpp",
+        "%{IncludeDir.VulkanSDK}",
+	}
+
+	includedirs
+	{
+        "",
+		"backends",
+		"%{wks.location}/external/glfw/include",
+        "%{IncludeDir.VulkanSDK}",
+	}
+
+    linkoptions
+    {
+        "-static-libstdc++",
+        "-static-libgcc"
+    }
+
+	links
+	{
+		"glfw",
+		"glad",
+        "mingw32",
+        "opengl32",
+        "gdi32",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
